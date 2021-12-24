bd61856df4a10ec90a48c7808d61e8deaf234335
Yamanipanuchi <yamanipanuchi@gmail.com>
2021-12-23 21:55:16 -0800

    Create startupwallets.sh

---
+++ startupwallets.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+forks="flax hddcoin staicoin stor btcgreen aedge flora spare chives maize wheat dogechia apple taco tad goji socks greendoge mint mogua lucky chaingreen shibgreen cryptodogeavocado >
+
+for fork in ${forks[@]}
+
+do
+        if [ "$( docker container inspect -f '{{.State.Status}}' coctohug-$fork )" == "exited" ]; then
+
+                echo "Starting $fork"
+                docker start coctohug-${fork[@]}
+                sleep 60
+
+                until [ "$( awk '{print $2}' /proc/loadavg | cut -d. -f1 )" -lt 4 ]; do
+
+                        echo "Waiting for CPU to calm down"
+                        sleep 10
+
+                done
+
+                else
+
+                echo "$fork = Already Running"
+
+        fi
+
+
+done
